c0113e52fd223a190d50ab335d6c980c8319efba
Antoni Danov <antoni.danov@gmail.com>
2023-08-24 16:14:34 +0200

    Create working method and connection with API

--- HappyWeather/WeatherWebAPI/Program.cs
+++ HappyWeather/WeatherWebAPI/Program.cs
@@ -1,15 +1,20 @@
+using dotenv.net;
+using WeatherWebAPI.Services.WeatherService;
+
 namespace WeatherWebAPI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            DotEnv.Load();
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
             builder.Services.AddControllers();
-
+            builder.Services.AddHttpClient();
+            builder.Services.AddScoped<IWeatherService, WeatherService>();
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
